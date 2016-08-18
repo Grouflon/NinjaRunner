32642befdc8d84fbffeef4417b4a63a3808822a9
Remi laptop <remi@uselessworks.com>
2016-08-18 22:28:12 +0200

    new sprite + CC tweaks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,24 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public float jumpStrength = 100.0f;
+    public float cruiseSpeed = 20.0f;
     public InputController input;
     public GameController game;
+    public GameObject graphic;
     public TouchGroundController groundChecker;
 
 	void Start ()
 	{
         m_rigidbody = GetComponent<Rigidbody2D>();
+        m_animator = graphic.GetComponent<Animator>();
 	}
 	
 	void Update ()
 	{
-	    if (input.GetJumpInput() && groundChecker.IsTouchingGround() && m_rigidbody.velocity.y < 0.1f)
+        m_animator.SetBool("IsTouchingGround", groundChecker.IsTouchingGround());
+        m_animator.SetFloat("SpeedRatio", game.gameSpeed / cruiseSpeed);
+
+        if (input.GetJumpInput() && groundChecker.IsTouchingGround() && m_rigidbody.velocity.y < 0.1f)
         {
             m_rigidbody.AddForce(new Vector2(0.0f, jumpStrength), ForceMode2D.Impulse);
         }
@@ -27,4 +33,5 @@ public class PlayerController : MonoBehaviour
 	}
 
     Rigidbody2D m_rigidbody;
+    Animator m_animator;
 }
