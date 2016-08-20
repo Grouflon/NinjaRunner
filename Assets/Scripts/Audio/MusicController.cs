6ef8fa406268eb31dd7cec5639d42f2dbb4c5d65
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2016-08-20 17:32:09 +0200

    placeholder music + loop system sample sync

---
+++ Assets/Scripts/Audio/MusicController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject musicLayers;
+    private AudioSource source;
+    private bool syncPointReached = false;
+    private int syncPointSample = 2822400;
+
+    public static MusicController instance;
+
+    // Singleton
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        
+        else if (instance != this)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        InstantiateMusic();
+    }
+
+    void Update()
+    {
+        if (source.timeSamples >= syncPointSample)
+            InstantiateMusic();
+    }
+
+    void InstantiateMusic()
+    {
+        GameObject go = (GameObject)Instantiate(musicLayers, new Vector3(0, 0, 0), Quaternion.identity);
+        go.transform.parent = this.gameObject.transform;
+        source = go.GetComponent<AudioSource>();
+    }
+
+
+}
