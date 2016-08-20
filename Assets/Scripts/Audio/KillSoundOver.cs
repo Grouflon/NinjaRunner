de10acbcbd852ed36106d74be3d780016142dd43
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2016-08-20 19:31:03 +0200

    3 layers of music playing

---
+++ Assets/Scripts/Audio/KillSoundOver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillSoundOver : MonoBehaviour {
+
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+	void Update () 
+    {
+        if (!source.isPlaying)
+            Destroy(this.gameObject);
+	}
+}
