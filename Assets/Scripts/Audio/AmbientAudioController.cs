5e74ab35a51886ca927c8ce5b4a70c6ecbcd3a1e
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2016-08-21 01:46:12 +0200

    Granular Audio engine + bird ambiance using the granular engine

---
+++ Assets/Scripts/Audio/AmbientAudioController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientAudioController : MonoBehaviour {
+
+    public static AmbientAudioController instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/Audio/GranularAudioPlayer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GranularAudioPlayer : MonoBehaviour {
+
+    [SerializeField] 
+    private GameObject soundObject;
+
+    [SerializeField]
+    [Range(0,30)]
+    private float minInitialDelay = 0f;
+    [SerializeField]
+    [Range(0,30)]
+    private float maxInitialDelay = 0f;
+    [SerializeField]
+    [Range(0,30)]
+    private float minSpawnTime = 0f;
+    [SerializeField]
+    [Range(0,30)]
+    private float maxSpawnTime = 0f;
+    [SerializeField]
+    [Range(0,1)]
+    private float maxRandomRadius = 0f;
+
+    private AudioSource source;
+    private bool hasPlayedOnce = false;
+
+
+    void Awake()
+    {
+        source = soundObject.GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(PlaySound());
+    }
+
+    Vector3 GetSpawnPosition()
+    {
+        float maxRandomDistance = source.maxDistance * maxRandomRadius;
+
+        float randomXOffset = Random.Range(0f, maxRandomDistance);
+        float randomYOffset = Random.Range(0f, maxRandomDistance);
+        float randomZOffset = Random.Range(0f, maxRandomDistance);
+
+        float randomX = Random.Range(Camera.main.transform.position.x - randomXOffset, Camera.main.transform.position.x + randomXOffset);
+        float randomY = Random.Range(Camera.main.transform.position.y - randomYOffset, Camera.main.transform.position.y + randomYOffset);
+        float randomZ = Random.Range(Camera.main.transform.position.z - randomZOffset, Camera.main.transform.position.z + randomZOffset);
+
+        Vector3 position = new Vector3(randomX, randomY, randomZ);
+        return position;
+    }
+
+    IEnumerator PlaySound()
+    {
+        if (!hasPlayedOnce)
+        {
+            yield return new WaitForSeconds(Random.Range(minInitialDelay, maxInitialDelay));
+            hasPlayedOnce = true;
+        }
+        else
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+        }
+
+        GameObject go = (GameObject)Instantiate(soundObject, GetSpawnPosition(), Quaternion.identity);
+        go.transform.parent = this.gameObject.transform;
+        StartCoroutine(PlaySound());
+    }
+
+}
