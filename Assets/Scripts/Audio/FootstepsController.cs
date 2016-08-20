3a08cf87c38c737d9ba92dedbb20d7b463782855
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2016-08-20 15:06:28 +0200

    Footstep controller

---
+++ Assets/Scripts/Audio/FootstepsController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepsController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject footstepGo;
+
+    public void PlayFootsteps() 
+    {
+        GameObject go = (GameObject)Instantiate(footstepGo, transform.position, Quaternion.identity);
+        go.transform.parent = this.gameObject.transform;
+	}
+
+}
