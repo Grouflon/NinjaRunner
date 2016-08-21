7f0db4e96e037ad4782a4d636df48c84da9a737a
Remi laptop <remi@uselessworks.com>
2016-08-21 16:10:15 +0200

    particles

---
+++ Assets/Scripts/ParticlesController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticlesController : MonoBehaviour
+{
+    public GameController game;
+
+	void Start ()
+	{
+        m_particles = GetComponent<ParticleSystem>();
+	}
+	
+	void Update ()
+	{
+        ParticleSystem.Particle[] _particles = new ParticleSystem.Particle[m_particles.particleCount];
+        m_particles.GetParticles(_particles);
+
+        for (int i = 0; i < _particles.Length; i++)
+        {
+            //Debug.Log(_particles[i].velocity);
+            _particles[i].velocity = new Vector3(_particles[i].velocity.x, _particles[i].velocity.y, game.GetGameSpeed() * 2.0f);
+        }
+
+        m_particles.SetParticles(_particles, _particles.Length);
+	}
+
+    ParticleSystem m_particles;
+}
