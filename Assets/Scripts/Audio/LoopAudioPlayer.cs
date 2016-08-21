3e7cd16423b19df833b8a0385f54769481155a5f
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2016-08-21 19:54:43 +0200

    add randomization in the way audio loops are played

--- Assets/Scripts/Audio/LoopAudioPlayer.cs
+++ Assets/Scripts/Audio/LoopAudioPlayer.cs
@@ -14,6 +14,17 @@ public class LoopAudioPlayer : MonoBehaviour {
     [Range (0f, 20f)]
     private float fadeOutTime;
 
+    [SerializeField]
+    [Range (0.1f, 3f)]
+    private float minPitch = 1f;
+
+    [SerializeField]
+    [Range (0.1f, 3f)]
+    private float maxPitch = 1f;
+
+    [SerializeField]
+    private bool randomStartPoint = false;
+
     private float targetVolume;
     private float volumeMultiplier;
     private float volumeMultiplierBeforeFadeOut;
@@ -29,11 +40,15 @@ public class LoopAudioPlayer : MonoBehaviour {
         source.loop = true;
 
         targetVolume = source.volume;
+        source.pitch = Random.Range(minPitch, maxPitch);
         source.volume = 0f;
     }
 
 	void Start () 
-    {        
+    {   
+        if (randomStartPoint)
+            source.timeSamples = (Random.Range(0, source.clip.samples));
+        
         source.Play();
         fadingIn = true;
 	}
